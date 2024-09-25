e0fffc416d1d585e9c2512fadc775693ccf0f142
Abhishek Gautam <gautamabhishek799@gmail.com>
2024-09-25 15:17:09 +0530

    project growth

---
+++ constants/data.js
@@ -0,0 +1,66 @@
+const userData = {
+  githubUsername: "gautam0066",
+  name: "Abhishek Gautam",
+  designation: "Front-End Developer",
+  avatarUrl: "/avatar.jpeg",
+  email: "abhishek.gautam@tothenew.com",
+  phone: "+91 9643662943",
+  address: "Delhi, New Delhi, India.",
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `Experienced working with teams to produce impactful, leading-edge websites that engage customers and deliver business results. Well-versed in design standards and user preferences.`,
+      `To seek and maintain a full-time position that offers professional challenges utilizing interpersonal skills, excellent time management, and problem-solving skills.`,
+      `Organized and dependable candidate successful at managing multiple priorities with a positive attitude. Willingness to take on added responsibilities to meet team goals.`
+    ],
+    currentProject: "Smart-TV",
+    currentProjectUrl: "https://vida.videoready.int.xp.irdeto.com/",
+  },
+  experience: [
+    {
+      title: "Software Engineer",
+      company: "TO The New Private Limited",
+      year: "Present",
+      companyLink: "https://www.tothenew.com/",
+      desc: "Developed and maintained React-based web applications with modern JavaScript technologies such as React Router, Redux. Implemented responsive user interfaces using SASS frameworks like Antd. Optimized application performance through profiling, testing, and debugging React code.",
+    },
+    {
+      title: "Trainee",
+      company: "TO The New Private Limited",
+      year: "2022",
+      companyLink: "https://www.tothenew.com/",
+      desc: "Developed and maintained React-based web applications with modern JavaScript technologies such as React Router, Redux.",
+    },
+    {
+      title: "Masters",
+      company: "Guru Gobind Singh Indraprastha University",
+      year: "2022",
+      companyLink: "http://www.ipu.ac.in/",
+      desc: "Major in Computer Application",
+    },
+    {
+      title: "Graduation",
+      company: "Guru Gobind Singh Indraprastha University",
+      year: "2019",
+      companyLink: "http://www.ipu.ac.in/",
+      desc: "Minor in Computer Application",
+    },
+    {
+      title: "High School",
+      company: "Delhi International School, New Delhi.",
+      year: "2015",
+      companyLink: "https://dis.ac.in/",
+      desc: "Subjects being commerce",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1SUPmV46Fam6uzElKADlM2qsgPJwDxGiP/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/gautam_22",
+    linkedin: "https://linkedin.com/in/abhishek-gautam-180b511b3",
+    github: "https://github.com/gautam0066",
+  },
+};
+
+export default userData;
---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import ContainerBlock from "../components/ContainerBlock";
+import LatestCode from "../components/LatestCode";
+import Hero from "../components/Hero";
+import getLatestRepos from "@lib/getLatestRepos";
+import userData from "@constants/data";
+
+export default function Home({ repositories }) {
+  return (
+    <ContainerBlock
+      title="Abhishek Gautam - Front-end Developer"
+      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+    >
+      <Hero />
+      <LatestCode repositories={repositories} />
+    </ContainerBlock>
+  );
+}
+
+export const getServerSideProps = async () => {
+  console.log(process.env.GITHUB_AUTH_TOKEN);
+  let token = process.env.GITHUB_AUTH_TOKEN;
+
+  const repositories = await getLatestRepos(userData, token);
+  // console.log("REPOSITORIES", repositories);
+
+  return {
+    props: {
+      repositories,
+    },
+  };
+};
